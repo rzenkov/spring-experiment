20851f0308f07160cfd91c95ff895275eabdc482
Roman Zenkov <zenkov.r@gmail.com>
2021-08-02 09:09:33 +0300

    Add user test

---
+++ src/test/java/ru/zenkovr/todotaskapi/model/UserTest.java
@@ -0,0 +1,11 @@
+package ru.zenkovr.todotaskapi.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserTest {
+
+    @Test
+    void isEmailConfirmed() {
+    }
+}
